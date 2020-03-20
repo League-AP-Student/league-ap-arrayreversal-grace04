6905723f64950f5ea8b044ba22d529ef9f4d4d2e
lxwgr <lxwgr@DESKTOP-L25KQUP>
2020-03-19 20:02:12 -0700

    not done

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -10,6 +10,13 @@ public class ArrayUtil {
 	*/
 	public static void reverseArray(int[] arr){
 		/* code goes here */
+		int[] arrnew = new int[arr.length];
+		for(int i=arr.length-1;i>-1;i--) {
+			for(int j=0;j<arr.length;j++) {
+				arrnew[j] = arr[i];
+			}
+		}
+		arr = arrnew;
 	}
 }
 
